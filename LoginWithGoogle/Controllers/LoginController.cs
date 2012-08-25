93132d6b56fbaee40eb4cb94da1ebbb6955284e2
Emiprotech <dilip@emiprotechnologies.com>
2012-08-25 16:47:20 +0530

    First commit

---
+++ LoginWithGoogle/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace LoginWithGoogle.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        # region googlelogin
+
+        public ActionResult LoginWithGoogle()
+        {
+            string authorizationUrl = string.Empty;
+            authorizationUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + System.Web.Configuration.WebConfigurationManager.AppSettings["GoogleClientId"] + "&redirect_uri=" + System.Web.Configuration.WebConfigurationManager.AppSettings["GoogleRedirectURL"] + "&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email &response_type=token";
+            return Redirect(authorizationUrl);
+        }
+
+        public ActionResult ReturnFromGoogle()
+        {
+            return View();
+        }
+
+        public ActionResult ReturnFromGoogle2()
+        {
+            var token = Request.QueryString["access_token"];
+            JObject je = new JObject();
+            HttpWebRequest req3 = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + token);
+            HttpWebResponse res3 = (HttpWebResponse)req3.GetResponse();
+            StreamReader streamReader3 = new StreamReader(res3.GetResponseStream());
+            string returnStr3 = streamReader3.ReadToEnd();
+            je = JObject.Parse(returnStr3);
+
+            try
+            {
+                ViewBag.SocialNetworkId = je["id"].ToString().Replace("\"", "");
+                ViewBag.FirstName = je["given_name"].ToString().Replace("\"", "");
+                ViewBag.LastName = je["family_name"].ToString().Replace("\"", "");
+                ViewBag.Email = je["email"].ToString().Replace("\"", "");
+                ViewBag.ProfilePicURL = je["picture"].ToString().Replace("\"", "");
+            }
+            catch { }
+            return View();
+        }
+
+        # endregion googlelogin
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 LoginWithGoogle_ASPNet_MVC3
 ===========================
+
+asp.net mvc3 web project which contains login with google and pull data from user's google account
+
+Steps: 1) Please add dll file[Newtonsoft.Json.Net35.dll] as reference to project 2) Create google console api[https://code.google.com/apis/console => Select API Access tab] in your google account & put appid & app secret values in web.config of this project 3) build and test this project.
+
+Best luck
